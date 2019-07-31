ef641a9a6bf44a0f4fc902a3cf7dfcac1d0422db
Alex Goncharov <80ka96@gmail.com>
2019-07-31 21:06:16 +0300

    v1.4 commint
    fix works
    add navH & navV
    add new work

---
+++ js/fp_settings.js
@@ -0,0 +1,7 @@
+var myFullpage = new fullpage('#fullpage', {
+    sectionsColor: ['rgba(0,0,0,0)', '#191919', '#ffffff', '#191919'],
+    navigation: true,
+    navigationPosition: 'right',
+    responsiveWidth: 1199,
+    responsiveHeight: 720
+});
